906c11082ce054496f2fe095046bf552a2b16ac8
DESKTOP-66EHK04\totski <andam.logicbaseinteractive@gmail.com>
2019-03-29 08:04:15 +0800

    sanitize code

--- public/js/vue-js/person.js
+++ public/js/vue-js/person.js
@@ -5,14 +5,12 @@ new Vue({
 
 	data: {
 		items: [],
+
 		item_id: '',
-		hasError: true,
-		hasDeleted: true,
-		hasAgeError: true,
-		showModal: false,
 		p_name: '',
 		p_age: '',    e_id: '',
 		p_profession: '',
+		
 		newItem: { 'name': '','age': '','profession': '' },
 	},
 
@@ -33,17 +31,13 @@ new Vue({
 			var _this = this;
 			var input = this.newItem;
 
-			if (input['name'] == '' || input['age'] == '' || input['profession'] == '' ) {
-				this.hasError = false;
-			} else {
-				this.hasError = true;
-				axios.post('/vueitems', input).then(function (response) {
-					_this.newItem = { 'name': '', 'age': '', 'profession': '' };
-					_this.getVueItems();
+			this.hasError = true;
+			axios.post('/vueitems', input).then(function (response) {
+				_this.newItem = { 'name': '', 'age': '', 'profession': '' };
+				_this.getVueItems();
 
-					toastr.success('Added successfully.');
-				});				
-			}
+				toastr.success('Added successfully.');
+			});
 		},
 
 		setVal(item_id, p_name,  p_age, p_profession) {
@@ -53,14 +47,14 @@ new Vue({
 			this.p_age = p_age;
 		},
 
-		editItem: function(){			
+		editItem: function(){		
 			var name = document.getElementById('p_name');
 			var age = document.getElementById('p_age');
 			var profession = document.getElementById('p_profession');
 
 			axios.post('/edititems/' + this.item_id, {p_name: name.value, p_age: age.value, p_profession: profession.value })
 			.then(response => {
-				this.getVueItems();			
+				this.getVueItems();
 
 				$('#'+this.item_id).modal('hide');
 				$('.modal-backdrop').remove();
@@ -72,7 +66,7 @@ new Vue({
 		deleteItem: function deleteItem(item) {
 			var _this = this;
 			axios.post('/vueitems/' + item.id).then(function (response) {
-				_this.getVueItems();				
+				_this.getVueItems();
 
 				toastr.success('Deleted successfully.');
 			});
